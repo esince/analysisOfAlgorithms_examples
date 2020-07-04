8b3e12cad04a61c24ec8815d6e14b019ac8e5758
esince <esinece6@gmail.com>
2020-07-04 16:42:43 +0300

    checking Bipartiteness in an undirected graph

--- checkBipartiteness.cpp
+++ checkBipartiteness.cpp
@@ -7,9 +7,11 @@
 
 #include <bits/stdc++.h>
 #define V 4
+// #define V 5
 
 using namespace std;
 
+//returns true if G[V][V] is bipartite
 bool isBipartite(int G[][V], int s){
     int colorArr[V]; //-1: no color, 0: blue, 1: red
     //initialization of colorArr[]
@@ -28,29 +30,49 @@ bool isBipartite(int G[][V], int s){
         q.pop();
 
         //1: red
+        //if there is a self loop
         if(G[top][top] == 1)
             return false;
+
         for(int x  = 0; x < V; ++x){
             if(colorArr[x] == -1 && G[top][x]){
                 colorArr[x] = 1 - colorArr[top];
                 q.push(x);
             }
             else if( colorArr[x] == colorArr[top] && G[top][x]){
-                
+                return false;
             }
         }
 
     }
+    return true;
 
 }
 
 //driver code
 int main(){
 
+//if number of vertices is odd in a cylce, then this is not bipartite 
+// 0-1-2-3-4-0 cyclic graph
+    // int graph[][V] = {{0,1,0,1,1},
+    //                    {1,0,1,0,0},
+    //                    {0,1,0,1,0},
+    //                    {1,0,1,0,1},
+    //                    {1,0,0,1,0}};
+
+//if a node have an edge to itself. it violates being bipartite
+//in this example, in the 4th node there is a self loop.
+    // int graph[][V] = {{0,1,0,1},
+    //                    {1,0,1,0},
+    //                    {0,1,0,1},
+    //                    {1,0,1,1}};
+
+//if number of vertices is even in a cycle, then this is bipartite. 
     int graph[][V] = {{0,1,0,1},
                        {1,0,1,0},
                        {0,1,0,1},
                        {1,0,1,0}};
+    isBipartite(graph, 0) ? cout << "Yes" : cout << "No"; 
     
     return 0;
 }
